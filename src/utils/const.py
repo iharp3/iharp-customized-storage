5f6c7e42e87adfd941741c9c8810e76eac9be7a2
Ana Uribe <uribe055@umn.edu>
2024-09-30 19:27:30 -0500

    add toy user input

--- src/utils/const.py
+++ src/utils/const.py
@@ -1,8 +1,24 @@
-RAW_DATA_PATH = "/data/customized/raw"
-AGG_DATA_PATH = "/data/customized/agg/"
+# RAW_DATA_PATH = "/data/customized/raw/"
+# AGG_DATA_PATH = "/data/customized/agg/"
 
-USER_INPUT_FILE_PATH = "/data/customized/user_input.csv"
+# USER_INPUT_FILE_PATH = "/data/customized/user_input.csv"
+
+RAW_DATA_PATH = "/home/uribe055/iharp-custumized-storage/data/raw/"
+AGG_DATA_PATH = "/home/uribe055/iharp-custumized-storage/data/agg/"
+
+USER_INPUT_FILE_PATH = "//home/uribe055/iharp-custumized-storage/data/user_input.csv"
 
 long_short_name_dict = {
     "2m_temperature": "t2m",
+}
+
+csv_col_names_dict = {
+    "var": "variable",
+    "res": "resolution",
+    "max_lat": "max_latitude",
+    "min_lat": "min_latitude",
+    "max_long": "max_longitude",
+    "min_long": "min_longitude",
+    "start_time": "start_time",
+    "end_time": "end_time"
 }
