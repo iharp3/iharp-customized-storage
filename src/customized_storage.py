36190b1c1e7738144f0c6aa92b77ea4d797cdf84
Ana Uribe <uribe055@umn.edu>
2024-09-30 19:28:43 -0500

    read in user input

--- src/customized_storage.py
+++ src/customized_storage.py
@@ -14,14 +14,45 @@ Author: Ana Uribe
 '''Imports'''
 import pandas as pd
 
-from utils.const import USER_INPUT_FILE_PATH, RAW_DATA_PATH, AGG_DATA_PATH
+from utils.const import (
+                        USER_INPUT_FILE_PATH, 
+                        RAW_DATA_PATH, 
+                        AGG_DATA_PATH,
+                        csv_col_names_dict,
+                        )
+
 
 '''Upload user input csv'''
-user_input = pd.read_csv(USER_INPUT_FILE_PATH)
+user_input_df = pd.read_csv(USER_INPUT_FILE_PATH)
 
 '''Get necessary API calls [and data processing steps?]'''
-unique_vars = user_input['variable']
+unique_vars = user_input_df[csv_col_names_dict["var"]].unique()
 
+for var in unique_vars:
+    # get all rows with this variable
+    var_df = user_input_df[user_input_df[csv_col_names_dict["var"]] == var]
+    
+    # get rows that need to be downloaded
+#     data_slices_df = get_data_slices(var_df)
+
+    # check if any lower resolutions are located (in time and space) within a higher resolution
+    res_hierarchy = {
+        'hour': ['day', 'month', 'year'],
+        'day': ['month', 'year'],
+        'month': ['year'],
+        'year': [] 
+        }
+    
+    for index, row in var_df.iterrows():
+        current_res = row[csv_col_names_dict["res"]]
+        lower_resolutions = res_hierarchy.get(current_res, [])
+
+        if lower_resolutions:
+                filtered_rows = var_df[var_df['resolution'].isin(lower_resolutions)]
+                
+    pass
+
+    
 
 
 
