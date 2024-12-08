e3796e032abfee23e0f97be9c439f1044d96c3d7
ana-e-uk <uribe055@umn.edu>
2024-12-07 22:59:44 -0600

    API generator script structure

--- src/ApiGenerator.py
+++ src/ApiGenerator.py
@@ -1,20 +1,51 @@
 """
 ApiGenerator.py: Creates and manages API calls to remote data repositories.
 """
+import numpy as np
 
-# Need to use YYY-MM-DD time
+from utils import get_data_path
 
 class API_Call:
-    # self  - all diff row values
-    #       - repository
-
-    # def   - era5_api_request()
-    #       - make_file()
-    #
     def __init__(self, row, name):
-        self.row = row  # user interest row
+        self.row = row  # user input row
         self.name = name    # file name
+
+    def get_year_range(self):
+        start_year = (self.row['start_time']).astype('M8[ns]').item().year
+        end_year = (self.row['end_time']).astype('M8[ns]').item().year
+
+        return [str(i) for i in range(int(start_year), int(end_year) + 1)]
+
+    def get_area_list(self):
+        n = self.row['max_lat_N']
+        w = self.row['min_long_W']
+        s = self.row['min_lat_S']
+        e = self.row['max_long_E']
+
+        return [n, w, s, e]
 
     def era5_api_request(self):
 
+        request = {"product_type": config.PRODUCT_TYPE,
+                   "variable": self.row['variable'],
+                   "year": self.get_year_range(),
+                   "month": config.MONTH_RANGE,
+                   "day": config.DAY_RANGE,
+                   "time": config.TIME_RANGE,
+                   "data_format": config.DATA_FORMAT,
+                   "download_format": config.DOWNLOAD_FORMAT,
+                   "area": get_area_list()
+        }
+
+        return request
+
     def make_file(self):
+        file_path = get_data_path(self.name)
+
+        dataset = config.DATASET
+        request = era5_api_request()
+
+        client = cdsapi.Client()
+
+        print(f"\tDownloading {self.row['variable']} data to {file_path}.")
+        client.retrieve(dataset,request,file_path)
--- src/config.py
+++ src/config.py
@@ -17,3 +17,12 @@ TIME = "valid_time"
 RAW_SP_RES = 0.25
 RAW_T_RES = "H"
 NUM_CHUNKS = 1000
+
+# era5 API request settings
+DATASET =  "reanalysis-era5-single-levels"
+PRODUCT_TYPE = ["reanalysis"]
+DATA_FORMAT = "netcdf"
+DOWNLOAD_FORMAT = "unarchived"
+MONTH_RANGE =  [str(i).zfill(2) for i in range(1, 13)],
+DAY_RANGE = [str(i).zfill(2) for i in range(1, 32)],
+TIME_RANGE = [f"{str(i).zfill(2)}:00" for i in range(0, 24)]
