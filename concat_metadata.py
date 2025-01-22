537384edc0c9a7ced899838a764805864f7ff194
uribe055@umn.edu <uribe055@umn.edu>
2025-01-21 23:16:37 -0600

    adding Yuchuans changes

---
+++ concat_metadata.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import argparse
+import os
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-S", "--storage_folder", type=str, help="Path to storage folder.")
+    args = parser.parse_args()
+    storage_folder = args.storage_folder
+
+    metadata_list = []
+
+    # for all folders in the storage folder
+    for folder in os.scandir(storage_folder):
+        if os.path.isdir(folder):
+            metadata = folder.path + "/0_post_metadata.csv"
+            # if metadata file exists
+            if os.path.exists(metadata):
+                print(metadata)
+                df = pd.read_csv(metadata)
+                print(len(df))
+                metadata_list.append(df)
+
+    # concatenate all metadata files
+    df_meta = pd.concat(metadata_list)
+    print(len(df_meta))
+    df_meta.to_csv("website_metadata_do_not_delete.csv", index=False)
---
+++ pp2025.py
@@ -0,0 +1,40 @@
+import argparse
+import pandas as pd
+
+"""
+python pp2025.py -D /data/iharp-customized-storage/storage/temperature
+"""
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-D", "--data_folder", type=str, help="Path to the data files folder.")
+    # parser.add_argument("-M", "--metadata", type=str, help="Path to existing metadata file.")
+    args = parser.parse_args()
+    data_folder = args.data_folder
+    # metadata = args.metadata
+    metadata = data_folder + "/metadata.csv"
+
+    print(f"Post-processing metadata: {metadata}...")
+    df_meta = pd.read_csv(metadata)
+    # add file_path
+    df_meta["file_path"] = data_folder + "/" + df_meta["file_name"]
+    # drop file_name
+    df_meta = df_meta.drop(columns=["file_name"])
+    # rename columns
+    df_meta = df_meta.rename(
+        columns={
+            "max_lat_N": "max_lat",
+            "min_lat_S": "min_lat",
+            "max_long_E": "max_lon",
+            "min_long_W": "min_lon",
+            "start_time": "start_datetime",
+            "end_time": "end_datetime",
+            "temporal_agg_type": "temporal_aggregation",
+            "spatial_agg_type": "spatial_aggregation",
+        }
+    )
+    # change literal conversion
+    df_meta["temporal_resolution"] = df_meta["temporal_resolution"].map(
+        {"H": "hour", "D": "day", "M": "month", "Y": "year"}
+    )
+    df_meta.to_csv(data_folder + "/0_post_metadata.csv", index=False)
+    print(f"Done. New metadata {data_folder}/0_post_metadata.csv")
